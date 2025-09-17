eb18059cfa34b043771e88f07c84b7129e9fd540
cxchajon <cxchajon@gmail.com>
2025-09-16 23:21:22 -0400

    Create fish-data.js

---
+++ fish-data.js
@@ -0,0 +1,36 @@
+// Keep all species data in this file.
+// It defines a global array: window.FISH_DATA
+window.FISH_DATA = [
+  { id:"neon_tetra", name:"Neon tetra", points:1.0, min:6, temp:[72,78], ph:[6.0,7.0] },
+  { id:"cardinal_tetra", name:"Cardinal tetra", points:1.2, min:6, temp:[74,82], ph:[5.0,7.0] },
+  { id:"ember_tetra", name:"Ember tetra", points:0.9, min:8, temp:[72,82], ph:[5.5,7.0] },
+  { id:"rummynose_tetra", name:"Rummy-nose tetra", points:1.4, min:8, temp:[75,82], ph:[5.5,6.8] },
+  { id:"black_skirt_tetra", name:"Black skirt tetra", points:2.0, min:6, temp:[70,82], ph:[6.0,7.5] },
+  { id:"harlequin_rasbora", name:"Harlequin rasbora", points:1.0, min:6, temp:[72,80], ph:[6.0,7.5] },
+  { id:"chili_rasbora", name:"Chili rasbora", points:0.6, min:10, temp:[72,80], ph:[5.0,7.0] },
+  { id:"zebra_danio", name:"Zebra danio", points:1.2, min:6, temp:[64,77], ph:[6.5,7.5] },
+  { id:"white_cloud", name:"White cloud mountain minnow", points:1.0, min:6, temp:[60,72], ph:[6.5,7.5] },
+  { id:"cherry_barb", name:"Cherry barb", points:2.0, min:6, temp:[73,81], ph:[6.0,7.5] },
+  { id:"tiger_barb", name:"Tiger barb", points:2.5, min:6, temp:[74,80], ph:[6.0,7.5] },      // nipper
+  { id:"guppy", name:"Guppy (male)", points:1.6, min:3, temp:[72,82], ph:[6.8,8.0] },        // mild nipper, long-fin
+  { id:"platy", name:"Platy", points:3.0, min:3, temp:[70,82], ph:[7.0,8.2] },
+  { id:"molly", name:"Molly", points:4.0, min:3, temp:[72,82], ph:[7.5,8.5] },
+  { id:"swordtail", name:"Swordtail", points:4.5, min:3, temp:[72,82], ph:[7.0,8.2] },
+  { id:"dwarf_gourami", name:"Dwarf gourami", points:5.0, min:1, temp:[77,82], ph:[6.0,7.5] },
+  { id:"honey_gourami", name:"Honey gourami", points:3.5, min:1, temp:[76,82], ph:[6.0,7.5] },
+  { id:"pearl_gourami", name:"Pearl gourami", points:7.0, min:1, temp:[76,82], ph:[6.0,8.0] },
+  { id:"betta_male", name:"Betta (male)", points:6.0, min:1, temp:[76,82], ph:[6.5,7.5] },
+  { id:"cory_small", name:"Corydoras (small)", points:2.2, min:6, temp:[72,78], ph:[6.0,7.5] },
+  { id:"cory_panda", name:"Corydoras panda", points:2.4, min:6, temp:[72,78], ph:[6.0,7.5] },
+  { id:"kuhli_loach", name:"Kuhli loach", points:2.5, min:5, temp:[75,82], ph:[5.5,7.0] },
+  { id:"otocinclus", name:"Otocinclus", points:1.2, min:6, temp:[72,79], ph:[6.0,7.5] },
+  { id:"bristlenose_pleco", name:"Bristlenose pleco", points:8.0, min:1, temp:[72,82], ph:[6.5,7.8] },
+  { id:"praecox_rainbow", name:"Dwarf rainbowfish (Praecox)", points:4.5, min:6, temp:[75,82], ph:[6.5,7.5] },
+  { id:"angelfish", name:"Angelfish", points:10.0, min:1, temp:[76,82], ph:[6.5,7.5] },
+  { id:"apistogramma", name:"Apistogramma (pair)", points:6.0, min:1, temp:[74,82], ph:[5.5,7.0] },
+  { id:"ram_cichlid", name:"Ram cichlid (German blue)", points:5.0, min:1, temp:[78,84], ph:[5.5,7.0] },
+  { id:"amano_shrimp", name:"Amano shrimp", points:0.5, min:3, temp:[68,78], ph:[6.5,7.5] },
+  { id:"cherry_shrimp", name:"Cherry shrimp", points:0.3, min:6, temp:[68,78], ph:[6.5,7.8] },
+  { id:"nerite_snail", name:"Nerite snail", points:0.4, min:1, temp:[72,78], ph:[7.2,8.4] },
+  { id:"mystery_snail", name:"Mystery snail", points:0.8, min:1, temp:[68,82], ph:[7.2,8.4] }
+];
