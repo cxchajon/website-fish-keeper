95ee1a2cff15ba2366547a781070b8694d28c21a
cxchajon <cxchajon@gmail.com>
2025-09-27 23:45:15 -0400

    Restore static asset versioning for stocking page

--- js/utils/version.js
+++
@@ -1,6 +0,0 @@
-export const APP_VERSION = (() => {
-  const t = (typeof window !== 'undefined' && window.__APP_BUILD__) || new Date();
-  const pad = (n) => String(n).padStart(2, '0');
-  const v = `${t.getFullYear()}${pad(t.getMonth() + 1)}${pad(t.getDate())}${pad(t.getHours())}${pad(t.getMinutes())}`;
-  return v;
-})();
