866f4c271279e6a98f17d6e06ddd92e01b1790b1
cxchajon <fishkeepinglifeco@gmail.com>
2025-10-26 22:39:52 -0400

    Remove legacy filtration summary card from prototype

--- prototype/assets/js/filtration.js
+++ prototype/assets/js/filtration.js
@@ -1,9 +1,5 @@
 (() => {
   if (!location.pathname.includes('/prototype/stocking-prototype.html')) return;
 
-  window.disableLegacyFiltrationSummary = true;
-
-  if (typeof window.renderFiltration === 'function') {
-    window.renderFiltration();
-  }
+  window.renderFiltration?.();
 })();
